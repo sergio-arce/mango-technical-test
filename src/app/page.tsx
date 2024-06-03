d6accb5ce578a09deb2a34b1643b89e8167c2122
Sergio Arce <arcesech30@gmail.com>
2024-06-03 22:41:10 +0200

    settings sass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Mango technical test</h1>
+    <main>
+      <h1>Mango technical</h1>
     </main>
   );
 }
